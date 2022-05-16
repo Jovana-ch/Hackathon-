35aa5beb312056d6317d2e60604c404335c62ad2
Miriam <miriamlunacarreon133@gmail.com>
2022-05-15 20:52:05 -0500

    Quitándo información no necesaria

--- src/client/components/Especies/EspeciesComponent.js
+++ src/client/components/Especies/EspeciesComponent.js
@@ -7,18 +7,6 @@ import ModalSeeEspecie from "./ModalSeeEspecie";
 import ModalAddEspecie from "./ModalAddEspecie";
 import { getAll } from "../../actions/especies";
 
-const data = [{title: "Tigre", url: "http://1.bp.blogspot.com/-_RF_XDfRVJg/TcGlZnIuFjI/AAAAAAAAAPA/Q7H9QBFNXZA/s1600/572737-1024x768-tiger.jpg",
-    description: "El tigre...", cause: "Caza excesiva"},
-    {title: "Tortuga", url: "https://www.atrapalo.com/houdinis/wp-content/uploads/2019/06/@zmachacek.jpg",
-    description: "La tortuga...", cause: "Caza excesiva"},
-    {title: "Tortuga caguama", url: "https://www.atrapalo.com/houdinis/wp-content/uploads/2019/06/@zmachacek.jpg",
-    description: "La tortuga caguama...", cause: "Caza excesiva"},
-    {title: "Tortuga2", url: "https://www.atrapalo.com/houdinis/wp-content/uploads/2019/06/@zmachacek.jpg",
-    description: "La tortuga 2...", cause: "Caza excesiva"},
-    {title: "Tortuga caguama2", url: "https://www.atrapalo.com/houdinis/wp-content/uploads/2019/06/@zmachacek.jpg",
-    description: "La tortuga caguama 2...", cause: "Caza excesiva"},
-    {title: "Tigre2", url: "http://1.bp.blogspot.com/-_RF_XDfRVJg/TcGlZnIuFjI/AAAAAAAAAPA/Q7H9QBFNXZA/s1600/572737-1024x768-tiger.jpg",
-    description: "El tigre 2...", cause: "Caza excesiva"}]
 export default class EspeciesComponent extends React.Component{
     constructor(){
         super();
--- src/client/components/Especies/ModalSeeEspecie.js
+++ src/client/components/Especies/ModalSeeEspecie.js
@@ -5,19 +5,19 @@ export default function ModalSeeEspecie({ show, setShow, data }) {
     const [name, setName] = React.useState("");
     const [imagen, setImagen] = React.useState("");
     const [description, setDescription] = React.useState("");
-    const [cause, setCause] = React.useState("");
+    //const [cause, setCause] = React.useState("");
  
     function onShow() {
         setName(data.name);
         setImagen(data.url);
         setDescription(data.Descripcion);
-        setCause(data.cause);
+        //setCause(data.cause);
     };
 
     function onHide() {
         setName("");
         setImagen("");
-        setCause("");
+        //setCause("");
         setDescription("");
         setShow();
     };
@@ -32,8 +32,6 @@ export default function ModalSeeEspecie({ show, setShow, data }) {
                     <img className='especieImg' src={imagen} alt="display image" style={{borderRadius: '5px'}}/>
                     <h3>{name}</h3><br/>
                     <label>{description}</label><br/>
-                    <h6>Causas por las que está en peligro de extinción: </h6>
-                    <label>{cause}</label>
                 </div>
             </Modal.Body>
             <Modal.Footer>
